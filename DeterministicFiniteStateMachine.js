0759be4d6f07b53e15e61059eb5e61c24f843e03
asala-hassan <60165523+asala-hassan@users.noreply.github.com>
2020-02-12 19:34:20 -0800

    Update Deterministic Finite State Machine

--- DeterministicFiniteStateMachine.js
+++ DeterministicFiniteStateMachine.js
@@ -3,7 +3,9 @@ export default class DeterministicFiniteStateMachine {
   /**
    */
   constructor({ transitions, startState, acceptStates }) {
-    throw 'IMPLEMENT constructor(description)';
+    this.transitions = transitions;
+    this.startState = startState;
+    this.acceptState;
   }
 
   /**
@@ -11,11 +13,18 @@ export default class DeterministicFiniteStateMachine {
    * @returns a string state name
    */
   transition(state, symbol) {
-    throw 'IMPLEMENT transition(state, symbol)';
+    return this.transitions[state][symbol];
+  }
+  
+  isAcceptState(state) {
+    return this.acceptStates.includes(state);
+    
   }
 
-  accepts(string, state = this._startState) {
-    throw 'IMPLEMENT FiniteStateMachine.accepts';
+  accepts(string, state = this.startState) {
+   const nextState = this.transition(state, string.charAt(0);
+   return (string.length === 0)? this.isAcceptState(state) :
+                                 this.accepts(string.substr(1), nextState);
   }
 
 }
